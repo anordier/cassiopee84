6e57b47965620b6686f2c18b3d606863cc8759b0
Alexis Nordier <nordier.alexis@gmail.com>
2022-04-28 14:01:39 +0200

    ça marche pas, retour à 8000

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
 //Importations
 const express = require('express')
 const app = express()
-const port = 143
+const port = 8000
 
 // Routes vers les fichiers statiques (html, css, js...)
 app.use(express.static('public'));
